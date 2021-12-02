788bce79528719c216f111a5721bdf5f6876fb51
Cconkers <rubenpv011992@gmail.com>
2021-12-02 09:09:05 +0100

    Mostly done

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { HeroesRoutingModule } from './heroes-routing.module';
+import { MaterialModule } from '../material/material.module';
+
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HeroesHomeComponent } from './pages/heroes-home/heroes-home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
-import { HeroesRoutingModule } from './heroes-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+import { imagenPipe } from './pipes/imagen.pipe';
 
 @NgModule({
   declarations: [
@@ -14,7 +20,9 @@ import { HeroesRoutingModule } from './heroes-routing.module';
     HeroeComponent,
     HeroesHomeComponent,
     ListadoComponent,
+    HeroeTarjetaComponent,
+    imagenPipe
   ],
-  imports: [CommonModule, HeroesRoutingModule],
+  imports: [CommonModule, HeroesRoutingModule, FlexLayoutModule,MaterialModule],
 })
 export class HeroesModule {}
--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class ListadoComponent implements OnInit {
 
-  constructor() { }
+@Input() heroes:Heroe[] = [];
+
+  constructor(private HeroesService: HeroesService) {}
 
   ngOnInit(): void {
+    this.HeroesService.getHeroes()
+    .subscribe(( heroes ) => this.heroes = heroes)
   }
-
-}
+  }
